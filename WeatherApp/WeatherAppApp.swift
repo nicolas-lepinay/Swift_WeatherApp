82b08c58b05c679cf86b6e176a29b7d6bf46d667
Nicolas Lépinay <nicolaslepinay@outlook.fr>
2023-05-12 03:34:30 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nicolas Lépinay on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
